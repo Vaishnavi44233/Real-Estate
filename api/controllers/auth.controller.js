f1595f0a9563fb44b1856496dcb8a9219818c8ce
Vaishnavi44233 <vaishnavimore207@gmail.com>
2025-04-18 00:17:16 +0530

    add signout functionality

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -4,16 +4,16 @@ import { errorHandler } from '../utils/error.js';
 import jwt from 'jsonwebtoken';
 
 export const signup =async (req, res, next) => {
-    const { username, email, password } = req.body;
-    const hashedPassword = bcryptjs.hashSync(password, 10);
-    const newUser = new User( {username, email, password: hashedPassword});
-    try {
-        await newUser.save()
-        res.status(201).json("User created successfully!");  
+  const { username, email, password } = req.body;
+  const hashedPassword = bcryptjs.hashSync(password, 10);
+  const newUser = new User( {username, email, password: hashedPassword});
+  try {
+    await newUser.save()
+    res.status(201).json("User created successfully!");  
 
-    } catch (error) {
-        next(error);
-    }
+  } catch (error) {
+    next(error);
+  }
 };
 
 export const signin = async (req, res, next) => {
@@ -32,7 +32,7 @@ export const signin = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
-  };
+};
 
 
 
@@ -70,4 +70,13 @@ export const signin = async (req, res, next) => {
     } catch (error) {
       next(error);
     }
+};
+
+export const signOut = async (req, res, next) => {
+  try {
+    res.clearCookie('access_token');
+    res.status(200).json('User has been logged out!');
+  } catch (error) {
+    next(error);
   }
+};
