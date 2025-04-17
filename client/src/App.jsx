1c3ab304dc15f10353200efd11ab5a767c2f141d
Vaishnavi44233 <vaishnavimore207@gmail.com>
2025-04-18 01:44:27 +0530

    create listing page functionality

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,7 @@ import About from './Pages/About';
 import Profile from './Pages/Profile';
 import Header from './components/Header';
 import PrivateRoute from './components/PrivateRoute';
+import CreateListing from './Pages/CreateListing';
 
 export default function App() {
   return <BrowserRouter>
@@ -18,6 +19,7 @@ export default function App() {
     <Route element={<PrivateRoute/>}>
       <Route path="/profile" element={<Profile />}/>
     </Route>
+    <Route path="/create-listing" element={<CreateListing />}/>
   </Routes>
   </BrowserRouter>;
 }
